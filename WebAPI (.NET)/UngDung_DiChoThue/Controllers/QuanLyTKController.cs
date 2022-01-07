86274a41b5c16971ded66bfab091b5376154ffe4
DESKTOP-T3NO52J\thanh <thanhle.16012000@gmail.com>
2022-01-07 17:55:16 +0700

    Init

---
+++ WebAPI (.NET)/UngDung_DiChoThue/Controllers/QuanLyTKController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace UngDung_DiChoThue.Controllers
+{
+    public class QuanLyTKController : ApiController
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=QL_DiChoThue;Integrated Security=True");
+
+        [Route("api/QuanLyTaiKhoan")]
+        // GET: QuanLyTK
+        public HttpResponseMessage Get()
+        {
+            string query = @"
+                        SELECT *
+                        FROM TaiKhoan TK";
+            return GetData(query);
+        }
+
+        [Route("api/QuanLyTaiKhoan/ChinhSua/{ID}")]
+        // PUT: QuanLyTK
+        public HttpResponseMessage UpdateTK(int ID, string vaitro, string trangthai)
+        {
+            try
+            {
+                string query = @"
+                        UPDATE TaiKhoan TK SET 
+                        TK.TrangThai=N'"+trangthai+ "', TK.VaiTro=N'" + vaitro + "'" +
+                        "WHERE TK.ID = '"+ID+"'";
+       
+                return UpdateData(query);
+            }
+            catch (Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+
+        // GET: QuanLyTK
+        public HttpResponseMessage GetData(string query)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            return Request.CreateResponse(HttpStatusCode.OK, dt);
+        }
+
+        // Post: QuanLyTK
+        public HttpResponseMessage UpdateData(string query)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(query, con);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            return Request.CreateResponse(HttpStatusCode.OK, dt);
+        }
+
+    }
+}
---
+++ WebAPI (.NET)/UngDung_DiChoThue/Model/TaiKhoan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace UngDung_DiChoThue.Model
+{
+    public partial class TaiKhoan
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string TrangThai { get; set; }
+        public double VaiTro { get; set; }
+    }
+}
